3e542da8001327fd09a30fab34fe4df5f6585f10
Wilhelm Behncke <behncke@sitegeist.de>
2019-02-21 15:25:25 +0100

    Add boilerplate for typescript and next.js

---
+++ typescript-next.js/build/static/development/pages/index.js
@@ -0,0 +1,86 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2Fhome%2Fbehncke%2FWorkspaces%2FPrivate%2Fes-starters%2Ftypescript-next.js%2Fpages%2Findex.tsx!./":
+/*!************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2Fhome%2Fbehncke%2FWorkspaces%2FPrivate%2Fes-starters%2Ftypescript-next.js%2Fpages%2Findex.tsx ***!
+  \************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var page = __webpack_require__(/*! ./pages/index.tsx */ "./src/pages/index.tsx")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.tsx */ "./src/pages/index.tsx", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.tsx */ "./src/pages/index.tsx")
+          next.router.update("/", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_a543f979d6475d4e1607 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_a543f979d6475d4e1607 */ "dll-reference dll_a543f979d6475d4e1607"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./src/pages/index.tsx":
+/*!*****************************!*\
+  !*** ./src/pages/index.tsx ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/home/behncke/Workspaces/Private/es-starters/typescript-next.js/src/pages/index.tsx";
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "Hello World!");
+});
+
+/***/ }),
+
+/***/ 1:
+/*!****************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2Fhome%2Fbehncke%2FWorkspaces%2FPrivate%2Fes-starters%2Ftypescript-next.js%2Fpages%2Findex.tsx ***!
+  \****************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2Fhome%2Fbehncke%2FWorkspaces%2FPrivate%2Fes-starters%2Ftypescript-next.js%2Fpages%2Findex.tsx! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2Fhome%2Fbehncke%2FWorkspaces%2FPrivate%2Fes-starters%2Ftypescript-next.js%2Fpages%2Findex.tsx!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_a543f979d6475d4e1607":
+/*!*******************************************!*\
+  !*** external "dll_a543f979d6475d4e1607" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_a543f979d6475d4e1607;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=index.js.map
