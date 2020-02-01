669abafe8b6303d2c92c5f1178d975f7bbbde746
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2020-02-01 09:33:21 +0100

    Remove typescript-marblejs boilerplate

--- typescript-marblejs/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "@marblejs/core";
-import { createServer } from "http";
-import httpListener from "./app";
-
-const httpListenerWithContext = httpListener
-    .run(createContext());
-
-export const httpServer = createServer(httpListenerWithContext).listen(3000);
