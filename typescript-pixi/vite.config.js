6a9d32cc7e2d1cd2c7381e0b463919760608e99b
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2021-09-03 22:59:23 +0200

    Fix vite.config.js of typescript-pixi

--- typescript-pixi/vite.config.js
+++ typescript-pixi/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     fs: {
-      // Allow serving files from one level up to the project root
       allow: ['/service-worker.js', '.']
     }
   }
